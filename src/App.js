8cbc01c613a600665c9c7273d8e4c43ab2cb62d5
kaiser101 <kaiser101@gmail.com>
2021-08-05 15:47:27 +0530

    Initial checkin

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import DataGridDemo from "./datagrid";
+
+function App() {
+  return (
+    <div className="App">
+      <DataGridDemo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/datagrid.js
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from "react";
+import { DataGrid } from "@material-ui/data-grid";
+const axios = require("axios");
+const moment = require("moment");
+const R = require("ramda");
+const _ = require("lodash");
+
+const { compose, map, filter, sortWith, ascend, prop } = R;
+
+const columns = [
+  { field: "name", headerName: "Name", width: 300 },
+  {
+    field: "address",
+    headerName: "Address",
+    width: 600,
+    editable: true,
+  },
+  {
+    field: "pincode",
+    headerName: "Pincode",
+    width: 140,
+    editable: true,
+  },
+  {
+    field: "date",
+    headerName: "Date",
+    // type: "number",
+    width: 120,
+    editable: true,
+  },
+  {
+    field: "available_capacity_dose2",
+    headerName: "Capacity",
+    type: "number",
+    width: 40,
+    editable: true,
+  },
+];
+
+const filter_session = (session) => {
+  return (
+    session.available_capacity_dose2 > 0 &&
+    session.min_age_limit === 18 &&
+    session.vaccine === "COVISHIELD"
+  );
+};
+
+const map_session = (session) => {
+  const {
+    center_id,
+    name,
+    address,
+    pincode,
+    date,
+    available_capacity_dose2,
+    slots,
+  } = session;
+  return {
+    id: center_id,
+    name,
+    address,
+    pincode,
+    date,
+    available_capacity_dose2,
+    // slots,
+  };
+};
+
+const sort_session = sortWith([ascend(prop("pincode")), ascend(prop("fee"))]);
+
+const DataGridDemo = () => {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    const getSlots = async () => {
+      let date = moment().add(1, "days").format("DD-MM-YYYY");
+      let response = await axios.get(
+        "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict/",
+        {
+          params: {
+            district_id: "395",
+            date,
+          },
+        }
+      );
+
+      const cowin_response = response.data;
+
+      const process_slots = compose(
+        sort_session,
+        map(map_session),
+        filter(filter_session),
+        prop("sessions")
+      );
+
+      let slots_for_second_sorted = process_slots(cowin_response);
+
+      return slots_for_second_sorted;
+    };
+
+    getSlots().then((response) => setRows(response));
+  }, []);
+
+  return (
+    <div style={{ height: 1200, width: "100%" }}>
+      <DataGrid rows={rows} columns={columns} pageSize={20} />{" "}
+    </div>
+  );
+};
+
+export default DataGridDemo;
